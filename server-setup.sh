0082df891876c33f2087a236be6c567804967e57
Onni <45150212+Onsqa@users.noreply.github.com>
2024-01-26 02:23:21 +0200

    Koodi fiksummaks :)

--- server-setup.sh
+++ server-setup.sh
@@ -1,236 +1,149 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+    then echo "Suorita tiedosto roottina"
+    exit
+fi
+
+shopt -s nocasematch
+ask_and_run() {
+    read -p "$1 (yes/no): " answer
+    if [[ "$answer" = "yes" ]] || [[ "$answer" = "y" ]]; then
+        shift
+        "${@}"
+    else
+        echo "Skipataan $1"
+    fi
+}
+
+ask_and_run "Haluatko disable password authentication ja root login?" disable_ssh_password_auth
+ask_and_run "Haluatko update ja upgrade järjestelmä tiedostot?" update_system_packages
+ask_and_run "Haluatko asentaa nginx, php 8.1, certbot?" install_nginx_php_certbot
+ask_and_run "Haluatko asentaa docker ja docker compose?" install_docker
+ask_and_run "Haluatko päivittää php limittejä?" update_php_limits
+ask_and_run "Haluatko lisätä käyttäjän www data ryhmään?" add_user_to_wwwdata
+ask_and_run "Haluatko aktivoida palomuurin" update_firewall
+ask_and_run "Haluatko asentaa MariaDB?" install_mariadb
+ask_and_run "Haluatko asentaa pikku ohjelmistoja mm speedtest, htop?" instalL_small_packages
+ask_and_run "Haluatko automatically clean files and config swappiness?" auto_clean_swappiness
+ask_and_run "Haluatko vaihtaa aikavyöhykkeen Helsinkiin?" change_timezone
+ask_and_run "Haluatko vaihtaa palvelimen nimen?" change_hostname
+ask_and_run "Haluatko käynnistää palvelimen uudelleen?" reboot
 
 
-# Ask user if they want to disable password authentication and root login
-read -p "Do you want to disable password authentication and root login? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
-read -p "Please enter your public key: " pub_key
-    sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-    sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+disable_ssh_password_auth() {
+    # Disable password authentication
+    read -p "Syötä Public key " pub_key
+    awk '/PasswordAuthentication yes/ {$0="PasswordAuthentication no"} 1' /etc/ssh/sshd_config > temp && mv temp /etc/ssh/sshd_config
+    # Disable root login
+    awk '/PermitRootLogin yes/ {$0="PermitRootLogin no"} 1' /etc/ssh/sshd_config > temp && mv temp /etc/ssh/sshd_config
+    # Restart ssh service
+    service ssh restart
+    # Append public key to the authorized_keys file
     echo "$pub_key" >> ~/.ssh/authorized_keys
-    sudo service ssh restart
-    else
-        echo "Skipping SSH configs"
-fi
-
-read -p "Do you want to update and upgrade system packages? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
-    sudo apt-get update -y 
-    sudo apt-get upgrade -y
-    echo "System packages updated and upgraded."
-    else
-        echo "System packages not updated or upgraded."
-fi
-
-read -p "Do you want to install nginx, php 8.1, certbot?" answer
-if [ "$answer" = "yes" ]; then
-    # Install Nginx and optimize it
-    # Install Nginx
-    sudo apt-get install nginx -y
-
-    # Optimize Nginx
-    sudo bash -c "cat >> /etc/nginx/nginx.conf" <<'EOF'
-worker_processes auto;
-events {
-    worker_connections 1024;
 }
-http {
-    keepalive_timeout 15;
+update_system_packages() {
+    yes |  apt-get update
+    yes |  apt-get upgrade 
 }
-EOF
 
-    # Test and reload Nginx
-    sudo nginx -t && sudo systemctl reload nginx
-    # Install PHP with addons
-    sudo apt-get install software-properties-common -y
-    sudo add-apt-repository ppa:ondrej/php
-    sudo apt-get update
-    sudo apt-get install -y php8.1-fpm
-    sudo apt-get install -y php8.1-mysql
-    sudo apt-get install -y php8.1-curl
-    sudo apt-get install -y php8.1-gd
-    sudo apt-get install -y php8.1-mbstring
-    sudo apt-get install -y php8.1-xml
-    sudo apt-get install -y php8.1-xmlrpc
-    sudo apt-get install -y php8.1-zip
-    sudo apt-get install -y php8.1-soap
-    sudo apt-get install -y php8.1-intl
-    sudo apt-get install -y php8.1-bcmath
-    sudo apt-get install -y php8.1-cli
-    sudo apt-get install -y php8.1-dev
-    sudo apt-get install -y php8.1-common
-    sudo apt-get install -y php8.1-json
-    sudo apt-get install -y php8.1-opcache
-    sudo apt-get install -y php8.1-readline
-    sudo apt-get install -y php8.1-imap
-    sudo apt-get install -y php8.1-imagick
-    sudo apt-get install -y php8.1-redis
-    sudo apt-get install -y php8.1-sqlite3
-    sudo apt-get install -y php8.1-xdebug
-    sudo apt-get install -y php8.1-xmlreader
-    sudo apt-get install -y php8.1-xmlwriter
-    sudo apt-get install -y php8.1-zmq
-    sudo apt-get install -y php8.1-ldap
-    sudo apt-get install -y php8.1-pgsql
-    sudo apt-get install -y php8.1-pdo
-    sudo apt-get install -y php8.1-pdo-mysql
-    sudo apt-get install -y php8.1-pdo-pgsql
-    sudo apt-get install -y php8.1-pdo-sqlite
-    
-    # Install Certbot for nginx
-    sudo apt-get install -y certbot
-    sudo apt-get install -y python3-certbot-nginx
-    sudo apt-get install -y python3-certbot-apache
+install_nginx_php_certbot() {
+    yes |  apt-get install -y nginx software-properties-common
+    yes |  add-apt-repository ppa:ondrej/php
+    yes |  apt-get update
+    yes | apt-get install -y php8.1-fpm php8.1-mysql php8.1-curl php8.1-gd php8.1-mbstring \
+            php8.1-xml php8.1-xmlrpc php8.1-zip php8.1-soap php8.1-intl \
+            php8.1-bcmath php8.1-cli php8.1-dev php8.1-common php8.1-json \
+            php8.1-opcache php8.1-readline php8.1-imap php8.1-imagick \
+            php8.1-redis php8.1-sqlite3 php8.1-xdebug php8.1-xmlreader \
+            php8.1-xmlwriter php8.1-zmq php8.1-ldap php8.1-pgsql php8.1-pdo \
+            php8.1-pdo-mysql php8.1-pdo-pgsql php8.1-pdo-sqlite
+}
 
-    # Nginx to systemctl
-    sudo systemctl enable nginx
-    else
-        echo "Skipping installing nginx, php 8.1, certbot"
-fi
-
-read -p "Do you want to install docker, docker-compose and add current user to docker group? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
+install_docker() {
     # Install Docker
-    sudo apt-get update
-    sudo apt-get install \
+    yes | apt-get update
+    yes |  apt-get install \
             apt-transport-https \
             ca-certificates \
             curl \
             gnupg \
             lsb-release
-    echo "Packages installed successfully."
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "Docker GPG key added."
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
         "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    echo "Docker repository added."
-    sudo apt-get update
-    echo "Package list updated."
-    sudo apt-get install docker-ce docker-ce-cli containerd.io
-    echo "Docker installed."
+        $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
+    yes |  apt-get install docker-ce docker-ce-cli containerd.io
     # Install Docker-Compose
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    echo "Docker Compose installed."
+    yes |  curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    yes |  chmod +x /usr/local/bin/docker-compose
     # User to docker group
-    sudo usermod -aG docker $USER
-    echo "User added to Docker group."
-    else
-        echo "Skipping installing Docker and Docker-Compose"
-fi
+    yes |  usermod -aG docker $USER
+}
 
-read -p "Do you want to edit php limits? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
-    # Edit PHP limits
-    sudo sed -i 's/upload_max_filesize = .*/upload_max_filesize = 2096M/' /etc/php/8.1/fpm/php.ini
-    sudo sed -i 's/post_max_size = .*/post_max_size = 100M/' /etc/php/8.1/fpm/php.ini
-    sudo sed -i 's/memory_limit = .*/memory_limit = 1024M/' /etc/php/8.1/fpm/php.ini
+update_php_limits() {
+    yes |  sed -i 's/upload_max_filesize = .*/upload_max_filesize = 2096M/' /etc/php/8.1/fpm/php.ini
+    yes |  sed -i 's/post_max_size = .*/post_max_size = 100M/' /etc/php/8.1/fpm/php.ini
+    yes |  sed -i 's/memory_limit = .*/memory_limit = 1024M/' /etc/php/8.1/fpm/php.ini
     # Restart PHP-FPM service
-    sudo service php8.1-fpm restart
+    yes |  service php8.1-fpm restart
     echo "PHP limits edited and PHP restarated."
-    else
-        echo "PHP limits not edited."
-fi
+}
 
-read -p "Do you want to add current user to www-data group? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
+add_user_to_wwwdata() {
     # Add current user to www-data group
-    sudo usermod -aG www-data $USER
-    echo "User added to www-data group."
+    yes |  usermod -aG www-data $USER
     # Change ownership of /var/www to www-data group
-    sudo chown -R :www-data /var/www
-    echo "Ownership of /var/www changed to www-data group."
+    yes |   chown -R :www-data /var/www
     # Set the setgid bit on /var/www so all new files are created with the www-data group
-    sudo chmod -R g+s /var/www
-    echo "Setgid bit set on /var/www."
-    else
-        echo "Skipping www-data group config"
-fi
+    yes |  chmod -R g+s /var/www
 
+}
 
+update_firewall() {
+    yes |  ufw enable
+    yes |  ufw allow OpenSSH
+    echo "Firewall enabled and SSH allowed."
 
-read -p "Do you want to configure firewall rules? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
-    # Configure Firewall
-    sudo ufw allow 3306/tcp
-    sudo ufw allow 'Nginx Full'
-    sudo ufw allow OpenSSH
-    echo "Firewall configured successfully"
-    else
-        echo "Skipping firewall configuration"
-fi
+}
 
-read -p "Do you want to install mariadb? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
-    read -sp "Enter the root password for MariaDB: " root_password
-    # Install MariaDB
-    sudo apt-get install mariadb-server -y
-    sudo systemctl enable mariadb
+install_mariadb(){
+    echo "Enter the root password for MariaDB: "
+    read -s root_password
+    yes |  apt-get install mariadb-server -y
+    yes |  systemctl enable mariadb
+    yes |  systemctl start mariadb
+    yes |  ufw allow 3306/tcp
 
-    sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('$root_password') WHERE User = 'root'"
-    sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-    sudo mysql -e "DELETE FROM mysql.user WHERE User=''"
-    sudo mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
-    sudo mysql -e "FLUSH PRIVILEGES"
-else
-    echo "Skipping mariadb install"
-fi
+    yes |  mysql -e "UPDATE mysql.user SET Password = PASSWORD('$root_password') WHERE User = 'root'"
+    yes |  mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+    yes |  mysql -e "DELETE FROM mysql.user WHERE User=''"
+    yes |  mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+    yes |  mysql -e "FLUSH PRIVILEGES"
 
-read -p "Do you want to optimize MariaDB? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
-    sudo bash -c "cat >> /etc/mysql/mariadb.conf.d/50-server.cnf" <<'EOF'
-[mysqld]
-# Set buffer pool size to 50-80% of your computer's memory
-innodb_buffer_pool_size=2048M
-# Set the log file size to about 25% of the buffer pool size
-innodb_log_file_size=512M
-# Other settings
-innodb_flush_log_at_trx_commit=1
-innodb_flush_method=O_DIRECT
-innodb_log_buffer_size=64M
-query_cache_size=64M
-query_cache_type=1
-EOF
-    sudo systemctl restart mariadb
-else
-    echo "Skipping MariaDB optimization"
-fi
+}
+install_small_packages(){
+    yes |  apt-get install -y zip unzip htop curl speedtest-cli
 
-read -p "Do you want to install small utility packages? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
-    # Install small utility packages
-    sudo apt-get install -y zip unzip
-    sudo apt-get install -y htop
-    sudo apt-get install -y curl
-    sudo apt-get install -y speedtest-cli
-    else 
-    echo "Skipping small utility packages"
-fi
+}
 
-read -p "Do you want to optimize system? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
-    sudo apt-get autoremove -y
-    sudo apt-get autoclean -y
-    echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
-    else
-    echo "Skipping optimization"
-fi
-read -p "Do you want to change server name? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
+auto_clean_swappiness() {
+    yes |  apt-get autoremove -y
+    yes |  apt-get autoclean -y
+    echo "vm.swappiness=10" |  tee -a /etc/sysctl.conf
+}
+change_timezone() {
+    timedatectl set-timezone Europe/Helsinki
+    echo "Timezone changed to Europe/Helsinki"
+}
+
+change_hostname() {
     read -p "Enter the new server name: " new_hostname
-    sudo hostnamectl set-hostname $new_hostname
+    yes |  hostnamectl set-hostname $new_hostname
     echo "Server name changed to $new_hostname."
-else
-    echo "Skipping server name change."
-fi
+}
 
-read -p "Do you want to change server timezone? (yes/no): " answer
-if [ "$answer" = "yes" ]; then
-    read -p "Enter the new timezone (e.g., 'America/Los_Angeles'): " new_timezone
-    sudo timedatectl set-timezone $new_timezone
-    echo "Server timezone changed to $new_timezone."
-else
-    echo "Skipping server timezone change."
-fi
-#wget -O setup.sh https://raw.githubusercontent.com/Onsqa/linux-setup-script/main/server-setup.sh && chmod +x server-setup.sh && ./server-setup.sh
+reboot() {
+    shutdown -r now
+    echo "Server rebooted."
+}
+shopt -u nocasematch
